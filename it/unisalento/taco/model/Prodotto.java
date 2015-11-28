81c078386a415e6a22d746787286420113ab4de3
goodnello <grislysquirrel@gmail.com>
2015-11-28 14:43:10 +0100

    Creazione corretta della classe

--- it/unisalento/taco/model/Prodotto.java
+++ it/unisalento/taco/model/Prodotto.java
@@ -2,19 +2,35 @@ package it.unisalento.taco.model;
 
 public class Prodotto {
 	
-	int id;
-	private String nome;
-	private String descrizione;
+	private final String nome;
+	private final String descrizione;
 	private double prezzo;
-	private Produttore produttore;
+	private final Produttore produttore;
 
-	String produttore(int id){
-		
-		String nomeProduttore = "Sconosciuto"; //default
-		
-		if(id == 1)
-			nomeProduttore = "SaporiPreziosi";
-		
-		return nomeProduttore;
+	public Prodotto(String nome, String descrizione, double prezzo, Produttore produttore){
+		this.nome = nome;
+		this.descrizione = descrizione;
+		this.prezzo = prezzo;
+		this.produttore = produttore;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getDescrizione() {
+		return descrizione;
+	}
+	
+	public double getPrezzo() {
+		return prezzo;
+	}
+	
+	public Produttore getProduttore() {
+		return produttore;
+	}
+	
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
 	}
 }
