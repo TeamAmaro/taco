975036f15d61912e08cd2aad15296aa2dd8168f8
goodnello <grislysquirrel@gmail.com>
2015-11-30 12:17:21 +0100

    Eliminata possibilità di creare progetto diciamo

--- it/unisalento/taco/model/CapoProgetto.java
+++ it/unisalento/taco/model/CapoProgetto.java
@@ -2,22 +2,8 @@ package it.unisalento.taco.model;
 
 public class CapoProgetto extends Utente {
 	
-	private Progetto progetto;
-	
 	public CapoProgetto(String nome, String cognome, String email){
 		super(nome,cognome,email);
-	}
-	
-	//void creaProgetto(){};
-	//void resocontoProgetto(){};
-	//void aggiungiDipendente(int idProgetto, int idDipendente){};
-	
-	public void setProgetto(String nome, double budget){
-		progetto = new Progetto(nome, budget);
-	}
-	
-	public Progetto getProgetto(){
-		return progetto;
 	}
 	
 }
