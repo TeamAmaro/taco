3f02fd30f3bfdc31a03c9a68d269fe4ffb7f4356
saucermann <daftpvnk@gmail.com>
2015-12-02 18:57:46 +0100

    primo prototipo di business class per dipendente

---
+++ it/unisalento/taco/business/DipendenteDelegate.java
@@ -0,0 +1,22 @@
+package it.unisalento.taco.business;
+
+import it.unisalento.taco.controller.DipendenteController;
+import it.unisalento.taco.model.Carrello;
+import it.unisalento.taco.model.Dipendente;
+import it.unisalento.taco.model.Prodotto;
+
+public class DipendenteDelegate {
+	private DipendenteDelegate instance ;
+	
+	public DipendenteDelegate getInstance(){
+		if(instance == null)
+			instance = new DipendenteDelegate();
+		return instance;
+	}
+	private DipendenteDelegate(){};
+	
+	public void acquistaProdotto(Prodotto prodotto, int quantita, DipendenteController controller){
+		Carrello carrello = controller.getCarrelloDipendente();
+		carrello.aggiungiProdotto(prodotto, quantita);
+	}
+}
