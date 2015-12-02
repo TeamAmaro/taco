bfe55586ee3ff398581552654ef36a9129ec9ebc
saucermann <daftpvnk@gmail.com>
2015-12-02 18:57:28 +0100

    prima implementazione

--- it/unisalento/taco/model/Carrello.java
+++ it/unisalento/taco/model/Carrello.java
@@ -1,19 +1,31 @@
 package it.unisalento.taco.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Carrello {
 
-	private ArrayList<Prodotto> listaProdotti;
+	private List<Prodotto> listaProdotti = new ArrayList<Prodotto>();
 	private double prezzoTotale;
 	
-	//implementare Singleton
+	private Carrello instance;
+	
+	public Carrello getInstance(){
+		if(instance == null)
+			instance = new Carrello();
+		return instance;
+	}
 	
 	//private double calcolaPrezzoTotale(){return new Double()};
 	//public Ordine generaOrdine() return new Ordine();};
 	
-	public Carrello() {
-		
+	private Carrello(){};
+	
+	public void aggiungiProdotto(Prodotto prodotto, int quantita){
+		for (int i = quantita; i > 0; i--){
+			listaProdotti.add(prodotto);
+		}
 	}
+	
 	
 }
