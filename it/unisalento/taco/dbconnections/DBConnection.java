97ec3e1b6d82c0846f6ed548fc1af8d62ad282b4
goodnello <grislysquirrel@gmail.com>
2015-12-04 13:08:47 +0100

    Aggiornato DBConnection usando il codice di Mynetti. Ora restituisce un ArrayList e non più un ResultSet. Fun Fact: Mynetti ha scritto il metodo "disconnetti()" ma non lo ha mai utilizzato. Forse andrebbe messo in un "finally" nel "try" dell'esecuzione delle query.

--- it/unisalento/taco/dbconnections/DBConnection.java
+++ it/unisalento/taco/dbconnections/DBConnection.java
@@ -1,45 +1,80 @@
 package it.unisalento.taco.dbconnections;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DBConnection {
 	
-	private Connection connect = null;
-	private Statement statement = null;
-	private ResultSet resultSet = null;
-	private String sqluser = "root";
-	private String sqlpsw = "root";
-	
-	public ResultSet queryDB(String query) throws Exception {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		    connect = DriverManager.getConnection("jdbc:mysql://localhost/tacodb", sqluser, sqlpsw);
-		    statement = connect.createStatement();
-		    resultSet = statement.executeQuery(query);
-		} catch (Exception e) {
-		      throw e;
-		    } finally {
-		    	//close();
-		    }
-		return resultSet;
-	}
-	
-	private void close() {
-		try {
-	      if (resultSet != null) {
-	        resultSet.close();
-	      }
+	   private static Connection db;
+	   private static boolean connesso;
+	   private static DBConnection instance;
 
-	      if (statement != null) {
-	        statement.close();
-	      }
+	   public static synchronized DBConnection getInstance() {
+		   if(instance == null)
+			   instance = new DBConnection();
+		   if(connesso != true)
+				connetti("tacodb", "root", "root");
+		   return instance;
+	   }
 
-	      if (connect != null) {
-	        connect.close();
-	      }
-	    } catch (Exception e) {
+	   private static boolean connetti(String nomeDB, String nomeUtente, String pwdUtente) {
 
-	    }
-	}
-	
+		   if(connesso) return true;
+
+	      try {
+	         Class.forName("com.mysql.jdbc.Driver");
+	         db = DriverManager.getConnection("jdbc:mysql://127.0.0.1/" + nomeDB + "?user=" + nomeUtente + "&password=" + pwdUtente);
+	         connesso=true;
+	         
+	      } catch (Exception e) {
+	    	  e.printStackTrace(); 
+	     }
+	      return connesso;
+	   }
+	   
+	   public ArrayList<String[]> queryDB(String query) {
+	      ArrayList<String[]> a = null;
+	      String [] record;
+	      int colonne = 0;
+	      try {
+	         Statement stmt = db.createStatement();
+	         ResultSet rs = stmt.executeQuery(query);
+	         a = new ArrayList<String[]>();
+	         ResultSetMetaData rsmd = rs.getMetaData();
+	         colonne = rsmd.getColumnCount();
+
+	         while(rs.next()) {
+	            record = new String[colonne];
+	            for (int i=0; i<colonne; i++) record[i] = rs.getString(i+1);
+	            a.add( (String[]) record.clone() );
+	         }
+	         rs.close();
+	         stmt.close();
+	      } catch (Exception e) { e.printStackTrace(); }
+
+	      return a;
+	   }
+
+	   public boolean updateDB(String query) {
+	      boolean risultato = false;
+	      try {
+	         Statement stmt = db.createStatement();
+	         stmt.executeUpdate(query);
+	         risultato = true;
+	         stmt.close();
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	         risultato = false;
+	      }
+	      return risultato;
+	   }
+
+	   private void disconnetti() {
+	      try {
+	         db.close();
+	         connesso = false;
+	      } catch (Exception e) { e.printStackTrace(); }
+	   }
+
+	   public boolean isConnesso() { return connesso; }
 }
--- it/unisalento/taco/view/TacoDemo.java
+++ it/unisalento/taco/view/TacoDemo.java
@@ -1,20 +1,20 @@
 package it.unisalento.taco.view;
 
 import it.unisalento.taco.model.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 
-import java.sql.ResultSet;
 import it.unisalento.taco.dbconnections.*;
 
 public class TacoDemo {
 
 	public static void main(String[] args) throws Exception{
 		
-		DBConnection conn = new DBConnection();
+		ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT * FROM utenti");
+		Iterator<String[]> i = result.iterator();
 		
-		ResultSet queryResult = conn.queryDB("SELECT nome,cognome,email FROM utenti");
-		queryResult.next();
-		
-		Dipendente dip = new Dipendente(queryResult.getString("nome"),queryResult.getString("cognome"),queryResult.getString("email"));
+		String[] riga = i.next();
+		Dipendente dip = new Dipendente(riga[1], riga[2], riga[3]);
 		
 		System.out.println("Dipendente");
 		System.out.println("Nome: " + dip.getNome());
