b30bdbdb65d6a5b287a079bedb914cf1980fc2cf
saucermann <daftpvnk@gmail.com>
2015-12-05 19:29:27 +0100

    Aggiunto primo prototipo di metodo business

--- it/unisalento/taco/business/DipendenteDelegate.java
+++ it/unisalento/taco/business/DipendenteDelegate.java
@@ -1,23 +1,30 @@
 package it.unisalento.taco.business;
 
-import it.unisalento.taco.controller.DipendenteController;
 import it.unisalento.taco.model.Carrello;
-import it.unisalento.taco.model.Dipendente;
 import it.unisalento.taco.model.Prodotto;
 
 public class DipendenteDelegate {
-	private DipendenteDelegate instance ;
+	private static DipendenteDelegate instance ;
 	
-	public DipendenteDelegate getInstance(){
+	public static DipendenteDelegate getInstance(){
 		if(instance == null)
 			instance = new DipendenteDelegate();
 		return instance;
 	}
 	private DipendenteDelegate(){};
 	
-	public void acquistaProdotto(Prodotto prodotto, int quantita, DipendenteController controller){
-		Carrello carrello = controller.getCarrelloDipendente();
-		carrello.aggiungiProdotto(prodotto, quantita);
+	public void aggiungiProdotto(Prodotto prodotto, int quantita){
+		System.out.println("Il delegato ha aggiunto " + quantita + " " + prodotto + "al carrello");
+		Carrello.getInstance().aggiungiProdotto(prodotto, quantita);
+	}
+	
+	public void rimuoviProdotto(Prodotto prodotto, int quantita){
+		System.out.println("Il delegato ha rimosso " + quantita + " " + prodotto.getNome() + " dal carrello");
+		Carrello.getInstance().rimuoviProdotto(prodotto, quantita);
+	}
+	
+	public void acquista(){
+		//Carrello.getInstance().acquista();
 	}
 	
 }
