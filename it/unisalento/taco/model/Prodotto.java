ea0990b1ec8601f0ba91e39994a5c8d1552699cc
saucermann <daftpvnk@gmail.com>
2015-12-05 14:40:19 +0100

    Prima versione con builder, listaFornitori ancora da testare

--- it/unisalento/taco/model/Prodotto.java
+++ it/unisalento/taco/model/Prodotto.java
@@ -1,20 +1,16 @@
 package it.unisalento.taco.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Prodotto {
 	
 	private final String nome;
-	private final String descrizione;
-	private final Categoria categoria;
-	private double prezzo;
+	private final double prezzo;
 	private final Produttore produttore;
-
-	public Prodotto(String nome, String descrizione, Categoria categoria, double prezzo, Produttore produttore){
-		this.nome = nome;
-		this.descrizione = descrizione;
-		this.categoria = categoria;
-		this.prezzo = prezzo;
-		this.produttore = produttore;
-	}
+	private String descrizione;
+	private Categoria categoria;
+	private List<Fornitore> listaFornitori = new ArrayList<Fornitore>();
 	
 	public String getNome() {
 		return nome;
@@ -36,7 +32,50 @@ public class Prodotto {
 		return produttore;
 	}
 	
-	public void setPrezzo(double prezzo) {
-		this.prezzo = prezzo;
+	public static class Builder{
+		private final String nome;
+		private final double prezzo;
+		private final Produttore produttore;
+		private String descrizione;
+		private Categoria categoria;
+		private List<Fornitore> listaFornitori = new ArrayList<Fornitore>();
+		
+		public Builder(String nome, double prezzo, Produttore produttore){
+			this.nome = nome;
+			this.prezzo = prezzo;
+			this.produttore = produttore;
+			this.descrizione = " ";
+			this.categoria = Categoria.CATEGORIA_0;
+		}
+		
+		public Builder descrizione(String val){
+			descrizione = val;
+			return this;
+		}
+		
+		public Builder categoria(Categoria val){
+			categoria = val;
+			return this;
+		}
+		
+		public Builder listaFornitori(Fornitore... args){
+			for(Fornitore val : args)
+				listaFornitori.add(val);
+			return this;
+		}
+		
+		public Prodotto build(){
+			return new Prodotto(this);
+		}
 	}
+	
+	private Prodotto(Builder build){
+		nome = build.nome;
+		prezzo = build.prezzo;
+		descrizione = build.descrizione;
+		produttore = build.produttore;
+		categoria = build.categoria;
+		listaFornitori = build.listaFornitori;
+	}
+	
 }
