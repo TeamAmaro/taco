81806c7e7087dfa3048a34660665ab0c5f6641b6
saucermann <daftpvnk@gmail.com>
2015-12-05 19:28:21 +0100

    Cambiamenti minori

--- it/unisalento/taco/model/Magazziniere.java
+++ it/unisalento/taco/model/Magazziniere.java
@@ -1,15 +1,17 @@
 package it.unisalento.taco.model;
 public class Magazziniere extends Utente {
 	
-	//int idMagazzino;
-	/*
-	void spedisciOrdine(int idOrdine){};
-	void rifornisciMagazzino(){};
-	*/
-	
 	private Magazzino magazzino;
 	
 	public Magazziniere(int id, String nome,String cognome,String email){
 		super(id,nome,cognome,email);
+	}
+	
+	public void setMagazzino(Magazzino magazzino){
+		this.magazzino = magazzino;
+	}
+	
+	public Magazzino getMagazzino(){
+		return magazzino;
 	}
 }
