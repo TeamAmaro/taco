94060c9c950764c7ff7e0b91191e1d2b84004b61
goodnello <grislysquirrel@gmail.com>
2015-12-06 16:30:00 +0100

    ProgettoDAOImpl - Cambiamenti vari alla query per ottenere anche il capo progetto

--- it/unisalento/taco/dao/ProgettoDAOImpl.java
+++ it/unisalento/taco/dao/ProgettoDAOImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 import it.unisalento.taco.dbconnections.DBConnection;
+import it.unisalento.taco.model.CapoProgetto;
 import it.unisalento.taco.model.Progetto;
 
 public class ProgettoDAOImpl implements ProgettoDAO{
@@ -16,14 +17,18 @@ public class ProgettoDAOImpl implements ProgettoDAO{
 
 	@Override
 	public Progetto getProgetto(int id) {
-		ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT id,nome,saldo,budget FROM progetti WHERE id = " + id);
+		ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT progetti.id,progetti.nome,progetti.saldo,progetti.budget,utenti.id,utenti.nome,utenti.cognome,utenti.email FROM utenti,progetti,capiprogetto WHERE capiprogetto.id_progetto = " + id + " AND capiprogetto.id_utente = utenti.id");
 		Iterator<String[]> i = result.iterator();
 		String[] riga = i.next();
 		double saldo = Double.parseDouble(riga[2]);
 		double budget = Double.parseDouble(riga[3]);
-		Progetto prog = new Progetto(id, riga[1], saldo, budget);
+		int capoProgID = Integer.parseInt(riga[4]);
+		CapoProgetto capoProg = new CapoProgetto(capoProgID, riga[5], riga[6], riga[7]);
+		Progetto prog = new Progetto(id, riga[1], capoProg, saldo, budget);
 		return prog;
 	}
+	
+	//public void addProgetto() {}
 
 	@Override
 	public void updateProgetto() {
