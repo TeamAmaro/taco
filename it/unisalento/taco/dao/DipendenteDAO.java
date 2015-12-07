d12874ee9e52d18957430cadec562edcf58e7cd1
goodnello <grislysquirrel@gmail.com>
2015-12-07 14:17:35 +0100

    DipendenteDAO - ProgettoDAO - I metodi getAll restituiscono correttamente degli array di oggetti

--- it/unisalento/taco/dao/DipendenteDAO.java
+++ it/unisalento/taco/dao/DipendenteDAO.java
@@ -1,14 +1,50 @@
 package it.unisalento.taco.dao;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
+import it.unisalento.taco.dbconnections.DBConnection;
 import it.unisalento.taco.model.Dipendente;
+import it.unisalento.taco.model.Progetto;
 
-public interface DipendenteDAO {
+public class DipendenteDAO {
 	
-	public ArrayList getAllDipendenti();
-	public Dipendente getDipendente(int id);
-	public void updateDipendente(Dipendente dipendente);
-	public void deleteDipendente(Dipendente dipendente);
+	public Dipendente[] getAllDipendenti() {
+		ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT utenti.* FROM dipendenti,utenti WHERE id_utente = utenti.id");
+		Iterator<String[]> i = result.iterator();
+		Dipendente[] dipendenti = new Dipendente[result.size()];
+		int j = 0;
+		
+		while(i.hasNext()) {
+			String[] riga = i.next();
+			int id = Integer.parseInt(riga[0]);
+			dipendenti[j++] = new Dipendente(id, riga[1], riga[2], riga[3]);
+		}
+		
+		return dipendenti;
+	}
+
+	public Dipendente getDipendente(int id) {
+		Dipendente dip;
+		ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT id,nome,cognome,email,id_progetto,id_sede FROM utenti,dipendenti WHERE id = id_utente AND id = " + id);
+		Iterator<String[]> i = result.iterator();
+		String[] riga = i.next();
+		dip = new Dipendente(id, riga[1], riga[2], riga[3]);
+		if(!riga[4].equals("null") || riga[4] != null) {
+			ProgettoDAO progDAO = new ProgettoDAO();
+			int id_progetto = Integer.parseInt(riga[4]);
+			dip.setProgetto(progDAO.getProgetto(id_progetto));
+		} 
+		return dip; 
+	}
+
+	public void updateDipendente(Dipendente dipendente) {
+		DBConnection.getInstance().updateDB("UPDATE utenti SET nome =" + dipendente.getNome() + ", cognome = " + dipendente.getCognome() + ", email = " + dipendente.getEmail() + " WHERE id = " + dipendente.getID());
+	}
+
+	public void deleteDipendente(Dipendente dipendente) {
+		// DA SCRIVERE QUANDO SERVIRA'
+		
+	}
 	
 }
--- it/unisalento/taco/dao/ProgettoDAO.java
+++ it/unisalento/taco/dao/ProgettoDAO.java
@@ -1,14 +1,66 @@
 package it.unisalento.taco.dao;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
+import it.unisalento.taco.dbconnections.DBConnection;
+import it.unisalento.taco.model.CapoProgetto;
 import it.unisalento.taco.model.Progetto;
 
-public interface ProgettoDAO {
+public class ProgettoDAO {
 	
-	public ArrayList getAllProgetto();
-	public Progetto getProgetto(int id);
-	public void updateProgetto();
-	public void deleteProgetto();
+	public Progetto[] getAllProgetto() {
+		
+		ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT * FROM progetti");
+		Iterator<String[]> i = result.iterator();
+		Progetto[] progetti = new Progetto[result.size()];
+		int j = 0;
+		CapoProgettoDAO capProgDAO = new CapoProgettoDAO();
+		
+		
+		while(i.hasNext()) {
+			
+			String[] riga = i.next();
+			int id = Integer.parseInt(riga[0]);
+			double saldo = Double.parseDouble(riga[2]);
+			double budget = Double.parseDouble(riga[3]);
+			
+			progetti[j] = new Progetto(id, riga[1], null, saldo, budget);
+			CapoProgetto capProg = capProgDAO.getCapoProgetto(progetti[j]);
+			progetti[j].setCapoProgetto(capProg);
+			j++;
+			
+			/*
+			CapoProgetto capProg = capProgDAO.getCapoProgetto(id);
+			progetti[j] = new Progetto(id, riga[1], capProg, saldo, budget);
+			capProg.setProgetto(progetti[j++]);*/
+		}
+		
+		return progetti;
+	}
+
+	public Progetto getProgetto(int id) {
+		ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT progetti.id,progetti.nome,progetti.saldo,progetti.budget,utenti.id,utenti.nome,utenti.cognome,utenti.email FROM utenti,progetti,capiprogetto WHERE capiprogetto.id_progetto = " + id + " AND capiprogetto.id_utente = utenti.id");
+		Iterator<String[]> i = result.iterator();
+		String[] riga = i.next();
+		double saldo = Double.parseDouble(riga[2]);
+		double budget = Double.parseDouble(riga[3]);
+		int capoProgID = Integer.parseInt(riga[4]);
+		CapoProgetto capoProg = new CapoProgetto(capoProgID, riga[5], riga[6], riga[7]);
+		Progetto prog = new Progetto(id, riga[1], capoProg, saldo, budget);
+		return prog;
+	}
+	
+	public void addProgetto() {}
+
+	public void updateProgetto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteProgetto() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
