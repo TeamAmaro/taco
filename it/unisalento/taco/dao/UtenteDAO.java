5da08d8f1cba3a495bb5c1ecf894362aa60dbd22
saucermann <daftpvnk@gmail.com>
2015-12-08 13:15:39 +0100

    Introdotto pattern singleton, soluzione a istanziazione utenti!

--- it/unisalento/taco/dao/UtenteDAO.java
+++ it/unisalento/taco/dao/UtenteDAO.java
@@ -8,8 +8,16 @@ import it.unisalento.taco.dbconnections.DBConnection;
 import it.unisalento.taco.model.Utente;
 
 public class UtenteDAO {
-
-	/*public List<Utente> getAllUtenti() {
+	
+	private static UtenteDAO instance;
+	public static UtenteDAO getInstance(){
+		if(instance == null)
+			instance = new UtenteDAO(){};
+		return instance;
+	}
+	private UtenteDAO (){};
+	
+	public List<? super Utente> getAllUtenti() {
 		ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT * FROM utenti");
 		Iterator<String[]> i = result.iterator();
 		List<Utente> listUtente = new ArrayList<>();
@@ -17,12 +25,31 @@ public class UtenteDAO {
 		while(i.hasNext()) {
 			String[] riga = i.next();
 			int id = Integer.parseInt(riga[0]);
-			//UTENTE NON PUO' ESSERE ISTANZIATO
-			Utente utente = new Utente(id, riga[1], riga[2], riga[3]);
-			listUtente.add(utente);
+			
+			/*
+			 * Controlla nel database l'informazione che contraddistingue utente nelle categorie 
+			 * dipendente - capoprogetto - magazziniere
+			 * 
+			 * Dopodiché basta istanziare DOPO il model corrispondente.
+			 * 
+			 * Attenzione:
+			 * non dobbiamo istanziare tutti i dati del database come model! Questa cosa si fa solo quando
+			 * è richiesto, ad esempio, se il superadmin ha bisogno di una lista completa di utenti e deve 
+			 * effettuare un'operazione su uno di essi.
+			 * 
+			 * Esempio di istanziazionee
+			 * Utente utente = new Dipendente(....);
+			 * 
+			 * Il compilatore potrebbe richiedere di castare l'oggetto nella classe specifica corrispondente.
+			 */
+			
+			
+			
+			//Utente utente = new Utente(id, riga[1], riga[2], riga[3]);
+			//listUtente.add(utente);
 		}
 		
 		return listUtente;
-	}*/
+	}
 	
 }
