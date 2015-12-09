a2b6a689944fc5dd6a6790e58ec7b57108c8c5ce
goodnello <grislysquirrel@gmail.com>
2015-12-09 18:58:08 +0100

    Restituisce l'inventario di un magazzino

--- it/unisalento/taco/dao/MagazzinoDAO.java
+++ it/unisalento/taco/dao/MagazzinoDAO.java
@@ -7,8 +7,10 @@
 package it.unisalento.taco.dao;
 
 import it.unisalento.taco.dbconnections.DBConnection;
+import it.unisalento.taco.model.Categoria;
 import it.unisalento.taco.model.Magazzino;
 import it.unisalento.taco.model.Prodotto;
+import it.unisalento.taco.model.Produttore;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -28,13 +30,19 @@ public class MagazzinoDAO {
     private MagazzinoDAO() {}
     
     public Map<Prodotto,Integer> getInventario(Magazzino magazzino) {
-        //DA SCRIVERE 
-        ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT * FROM progetti");
+
+        String uberQuery = "SELECT produttori.nome,prodotti.nome,prodotti.categoria,prodotti.descrizione,prodotti.prezzo,prod_mag.quantita FROM prodotti,prod_mag,produttori,magazzini WHERE prod_mag.id_magazzino = " + magazzino.getID() + " AND prod_mag.id_magazzino = magazzini.id AND prodotti.id = prod_mag.id_prodotto AND produttori.id_prodotto = prodotti.id";
+        ArrayList<String[]> result = DBConnection.getInstance().queryDB(uberQuery);
         Iterator<String[]> i = result.iterator();
         Map<Prodotto,Integer> inventario = new LinkedHashMap<>();
         
-        //inventario.put(prodottox,quantitax);
-        
+        while(i.hasNext()) {
+            String[] riga = i.next();
+            double prezzo = Double.parseDouble(riga[4]);
+            Prodotto prodotto = new Prodotto.Builder(riga[1], prezzo, Produttore.PRODUTTORE_A).categoria(Categoria.CATEGORIA_D).descrizione(riga[3]).build();
+            int quantita = Integer.parseInt(riga[5]);
+            inventario.put(prodotto,quantita);
+        }
         return inventario;
     }
     
