bd6dcf082dab79df9a1e3a5c09832ad1a5e5ceca
saucermann <daftpvnk@gmail.com>
2015-12-12 22:55:48 +0100

    //

--- it/unisalento/taco/model/Fornitore.java
+++ it/unisalento/taco/model/Fornitore.java
@@ -2,37 +2,37 @@ package it.unisalento.taco.model;
 
 public enum Fornitore {
 	
-	FORNITORE_A("Fornitore A"),
-	FORNITORE_B("Fornitore B"),
-	FORNITORE_C("Fornitore C"),
-        FORNITORE_D("Fornitore D"),
-	FORNITORE_0("Sconosciuto");
-	
-	private final String nome;
-	
-	private Fornitore(String nome){
-		this.nome = nome;
-	}
-	
-	public String nome() {
-		return nome;
-	}
-	
-	@Override public String toString(){
-		return nome;
-	}
-        
-        public static Fornitore parseFornitore(String fornitore){
-            if(fornitore.equals(FORNITORE_A.nome))
-                return FORNITORE_A;
-            else if(fornitore.equals(FORNITORE_B.nome))
-                return FORNITORE_B;
-            else if(fornitore.equals(FORNITORE_C.nome))
-                return FORNITORE_C;
-            else if(fornitore.equals(FORNITORE_D.nome))
-                return FORNITORE_D;
-            else
-                return FORNITORE_0;
-        }
+    FORNITORE_A("Fornitore A"),
+    FORNITORE_B("Fornitore B"),
+    FORNITORE_C("Fornitore C"),
+    FORNITORE_D("Fornitore D"),
+    FORNITORE_0("Sconosciuto");
+
+    private final String nome;
+
+    private Fornitore(String nome){
+            this.nome = nome;
+    }
+
+    public String nome() {
+            return nome;
+    }
+
+    @Override public String toString(){
+            return nome;
+    }
+
+    public static Fornitore parseFornitore(String fornitore){
+        if(fornitore.equals(FORNITORE_A.nome))
+            return FORNITORE_A;
+        else if(fornitore.equals(FORNITORE_B.nome))
+            return FORNITORE_B;
+        else if(fornitore.equals(FORNITORE_C.nome))
+            return FORNITORE_C;
+        else if(fornitore.equals(FORNITORE_D.nome))
+            return FORNITORE_D;
+        else
+            return FORNITORE_0;
+    }
 	
 }
