6eb9885980bc7d4df022faa45d196feefed46959
goodnello <grislysquirrel@gmail.com>
2015-12-12 18:30:29 +0100

    Il metodo per creare un CapoProgetto tramite un ID è da completare

--- it/unisalento/taco/dao/CapoProgettoDAO.java
+++ it/unisalento/taco/dao/CapoProgettoDAO.java
@@ -24,6 +24,14 @@ public class CapoProgettoDAO {
 		return capoProg;
 	}
 	
+        //DA SCRIVERE
+        /*public CapoProgetto getCapoProgetto(int id){
+            CapoProgetto capoProgetto;
+            ArrayList<String[]> result = DBConnection.getInstance().queryDB(" SELECT utenti.id,utenti.nome,utenti.cognome,utenti.email,progetti.id,progetti.nome FROM utenti,progetti,capiprogetto WHERE utenti.id = " + id + " AND utenti.id = id_utente AND id_progetto = progetti.id");
+            Iterator<String[]> i = result.iterator();
+            String[] riga = i.next();
+        }*/
+        
 	/*//OTTIENE IL CAPOPROGETTO TRAMITE L'ID DEL PROGETTO
 	public CapoProgetto getCapoProgetto(int idProgetto) {
 		ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT utenti.* FROM progetti,capiprogetto,utenti WHERE id_progetto = progetti.i AND id_progetto = " + idProgetto + " AND id_utente = utenti.id");
