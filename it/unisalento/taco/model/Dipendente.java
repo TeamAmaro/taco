872d56d189e8c57955acc6dae79fe670c3d31a80
saucermann <daftpvnk@gmail.com>
2015-12-12 22:56:42 +0100

    Sede è un parametro obbligatorio

--- it/unisalento/taco/model/Dipendente.java
+++ it/unisalento/taco/model/Dipendente.java
@@ -1,32 +1,34 @@
 package it.unisalento.taco.model;
+
 public class Dipendente extends Utente {
 
-	private Progetto progetto;
-	private Sede sede;
+	private Progetto progetto; 
+	private Sede sede;  
 	private Carrello carrello;
 
-	//DIPENDENTE BASE
-	public Dipendente(int id, String nome, String cognome, String email){
+	//Costruttore di base per il dipendente
+	public Dipendente(int id, String nome, String cognome, String email, Sede sede){
 		super(id,nome,cognome,email);
+                this.sede = sede;
 	}
 
-	public Dipendente(int id, String nome, String cognome, String email, Progetto progetto, Sede sede, Carrello carrello){
+	public Dipendente(int id, String nome, String cognome, String email, Sede sede, Progetto progetto, Carrello carrello){
 		super(id,nome,cognome,email);
 		this.progetto = progetto;
 		this.sede = sede;
-		this.carrello = carrello.getInstance();
+		this.carrello = carrello;
 	}
 
 	public void setProgetto(Progetto progetto) {
 		this.progetto = progetto;
 	}
 
-	public Progetto getProgetto() {
-		return progetto;
-	}
-
 	public void setSede(Sede sede) {
 		this.sede = sede;
+	}
+        
+	public Progetto getProgetto() {
+		return progetto;
 	}
 
 	public Sede getSede() {
