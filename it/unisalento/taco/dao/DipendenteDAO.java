1f19f3e37762036ca7ab2793bcd6521649486ef6
saucermann <daftpvnk@gmail.com>
2015-12-12 23:00:54 +0100

    Adeguato al nuovo costruttore. Deve essere controllato!!

--- it/unisalento/taco/dao/DipendenteDAO.java
+++ it/unisalento/taco/dao/DipendenteDAO.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import it.unisalento.taco.dbconnections.DBConnection;
 import it.unisalento.taco.model.Dipendente;
+import it.unisalento.taco.model.Sede;
 
 public class DipendenteDAO {
 	
@@ -19,14 +20,15 @@ public class DipendenteDAO {
 	private DipendenteDAO(){};
 	
 	public List<Dipendente> getAllDipendenti() {
-		ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT utenti.* FROM dipendenti,utenti WHERE id_utente = utenti.id");
+		ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT utenti.*, id_sede FROM dipendenti,utenti WHERE id_utente = utenti.id");
 		Iterator<String[]> i = result.iterator();
 		List<Dipendente> listDipendente = new ArrayList<>();
 		
 		while(i.hasNext()) {
 			String[] riga = i.next();
 			int id = Integer.parseInt(riga[0]);
-			Dipendente dipendente = new Dipendente(id, riga[1], riga[2], riga[3]);
+                        //DA CONTROLLARE!!!
+			Dipendente dipendente = new Dipendente(id, riga[1], riga[2], riga[3], Sede.parseSede(riga[5]));
 			listDipendente.add(dipendente);
 		}
 		
@@ -38,7 +40,7 @@ public class DipendenteDAO {
 		ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT id,nome,cognome,email,id_progetto,id_sede FROM utenti,dipendenti WHERE id = id_utente AND id = " + id);
 		Iterator<String[]> i = result.iterator();
 		String[] riga = i.next();
-		dip = new Dipendente(id, riga[1], riga[2], riga[3]);
+		dip = new Dipendente(id, riga[1], riga[2], riga[3], Sede.parseSede(riga[5]));
 		if(!riga[4].equals("null") || riga[4] != null) {
 			ProgettoDAO progDAO = ProgettoDAO.getInstance();
 			int id_progetto = Integer.parseInt(riga[4]);
