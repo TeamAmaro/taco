defe46f5f45d1e7f08aa4414671b7311a791fdac
saucermann <daftpvnk@gmail.com>
2015-12-13 17:15:43 +0100

    Eliminata sede da capoprogetto

--- it/unisalento/taco/model/CapoProgetto.java
+++ it/unisalento/taco/model/CapoProgetto.java
@@ -5,26 +5,19 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class CapoProgetto extends Utente {
-    private Sede sede;
     private Set<Progetto> listaProgetti = new LinkedHashSet<>();
 
-    public CapoProgetto(int id, String nome, String cognome, String email, Sede sede){
+    public CapoProgetto(int id, String nome, String cognome, String email){
         super(id,nome,cognome,email);
-        this.sede = sede;
     }
 
-    public CapoProgetto(int id, String nome, String cognome, String email, Sede sede, Progetto... args) {
+    public CapoProgetto(int id, String nome, String cognome, String email, Progetto... args) {
         super(id,nome,cognome,email);
-        this.sede = sede;
         listaProgetti.addAll(Arrays.asList(args));
     }
-
-    public void setSede(Sede sede){
-        this.sede = sede;
-    }
-
-    public Sede getSede(){
-        return sede;
+    
+    public void setListaProgetti(Set<Progetto> listaProgetti){
+        this.listaProgetti = listaProgetti;
     }
 
     public Set<Progetto> getListaProgetti() {
