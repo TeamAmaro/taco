e1dca8d90b3687af7d8de6b5baa5245ea3a631b3
goodnello <grislysquirrel@gmail.com>
2015-12-13 18:26:39 +0100

    Miglioramenti al codice, ora ottiene la quantita di un prodotto in un magazzino e in quali magazzini un prodotto è presente

--- it/unisalento/taco/dao/MagazzinoDAO.java
+++ it/unisalento/taco/dao/MagazzinoDAO.java
@@ -11,6 +11,7 @@ import it.unisalento.taco.model.Categoria;
 import it.unisalento.taco.model.Magazzino;
 import it.unisalento.taco.model.Prodotto;
 import it.unisalento.taco.model.Produttore;
+import it.unisalento.taco.model.Sede;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -31,43 +32,51 @@ public class MagazzinoDAO {
     
     private MagazzinoDAO(){}
     
+    //RESTITUISCE TUTTI I PRODOTTI DI UN MAGAZZINO
     public Map<Prodotto,Integer> getInventario(Magazzino magazzino) {
 
-        String uberQuery = "SELECT produttori.nome,prodotti.id,prodotti.nome,prodotti.categoria,prodotti.descrizione,prodotti.prezzo,prod_mag.quantita FROM prodotti,prod_mag,produttori,magazzini WHERE prod_mag.id_magazzino = " + magazzino.getID() + " AND prod_mag.id_magazzino = magazzini.id AND prodotti.id = prod_mag.id_prodotto AND produttori.id_prodotto = prodotti.id";
+        String uberQuery = "SELECT produttori.nome,prodotti.*,prod_mag.quantita FROM prodotti,prod_mag,produttori,magazzini WHERE prod_mag.id_magazzino = " + magazzino.getID() + " AND prod_mag.id_magazzino = magazzini.id AND prodotti.id = prod_mag.id_prodotto AND produttori.id_prodotto = prodotti.id";
         ArrayList<String[]> result = DBConnection.getInstance().queryDB(uberQuery);
         Iterator<String[]> i = result.iterator();
         Map<Prodotto,Integer> inventario = new LinkedHashMap<>();
         
         while(i.hasNext()) {
             String[] riga = i.next();
-            double prezzo = Double.parseDouble(riga[5]);
-            Prodotto prodotto = new Prodotto.Builder(Integer.parseInt(riga[1]), riga[2], prezzo, Produttore.parseProduttore(riga[0])).categoria(Categoria.parseCategoria(riga[3])).descrizione(riga[4]).build();
+            Prodotto prodotto = new Prodotto.Builder(Integer.parseInt(riga[1]), riga[2], Double.parseDouble(riga[5]), Produttore.parseProduttore(riga[0])).categoria(Categoria.parseCategoria(riga[3])).descrizione(riga[4]).build();
             int quantita = Integer.parseInt(riga[6]);
-            if(quantita == 0) {
+            if(quantita == 0)
                 continue;
-            }
             inventario.put(prodotto,quantita);
         }
         return inventario;
     }
     
-    /*public int getQuantita(Prodotto prodotto) {
-        //DA SCRIVERE
-        ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT * FROM progetti");
+    //RESTITUISCE LA QUANTITA DI UN PRODOTTO IN UN MAGAZZINO
+    public int getQuantita(Magazzino magazzino, Prodotto prodotto) {
+
+        ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT quantita FROM prod_mag WHERE id_magazzino = " + magazzino.getID() + " AND id_prodotto = " + prodotto.getID());
         Iterator<String[]> i = result.iterator();
-        String[] riga = i.next();
-        int quantita = Integer.parseInt(riga[0]);
+        int quantita = 0;
+        if(i.hasNext()) {
+            String[] riga = i.next();
+            quantita = Integer.parseInt(riga[0]);
+        }
         return quantita;
-    }*/
+    }
     
+    //RESTITUISCE I MAGAZZINI IN CUI E' PRESENTE UN PRODOTTO
     public Set<Magazzino> cercaProdotto(Prodotto prodotto) {
-        /*
-        RESTITUISCO I MAGAZZINI CHE CONTENGONO IL PRODOTTO
-        
-        String uberQuery = "select magazzini.nome from magazzini, prod_mag, prodotti where prodotti.id = " + prodotto.getID() + " AND prod_mag.id_prodotto = prodotti.id AND prod_mag.id_magazzino = magazzini.id";
-        ArrayList<String[]> = DBConnection.getInstance().queryDB(uberQuery);
-        */
+        String uberQuery = "SELECT * from magazzini, prod_mag, prodotti WHERE prodotti.id = "
+                + prodotto.getID() + " AND prod_mag.id_prodotto = prodotti.id AND prod_mag.id_magazzino = magazzini.id";
+        ArrayList<String[]> result = DBConnection.getInstance().queryDB(uberQuery);
+        Iterator<String[]> i = result.iterator();
         Set<Magazzino> magazzini = new LinkedHashSet<>();
+        
+        while(i.hasNext()){
+            String[] riga = i.next();
+            Magazzino magazzino = new Magazzino(Integer.parseInt(riga[0]), riga[1], Sede.parseSede(riga[2]));
+            magazzini.add(magazzino);
+        }
         return magazzini;
     }
     
