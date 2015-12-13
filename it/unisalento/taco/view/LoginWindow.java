97462b3f213a0d2145ff75b39c7b273772395753
saucermann <daftpvnk@gmail.com>
2015-12-13 19:24:31 +0100

    Primo esempio di finestra di login

--- it/unisalento/taco/view/LoginWindow.java
+++ it/unisalento/taco/view/LoginWindow.java
@@ -1,38 +1,87 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package it.unisalento.taco.view;
 
+import it.unisalento.taco.actionListeners.LoginButtonListener;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-/**
- *
- * @author tomma
- */
 public class LoginWindow extends JFrame{
+            
+    private JPanel centro;
+    private JPanel nord;
+    private JPanel sud;
+
+    private JTextField emailField;
+    private JPasswordField passwordField;
+    private JLabel emailLabel;
+    private JLabel passwordLabel;
+    private JButton loginButton;
+    
+    private Dimension dim;
+
     public LoginWindow(){
-        super("Login Window");
-        JTextField usernameField = new JTextField("Inserisci il tuo username");
-        JPasswordField passwordField = new JPasswordField("Inserisci la tua password");
+        super("TACO");
         Container c = this.getContentPane();
-        c.setLayout(new GridLayout(3,5));
-        c.add(usernameField);
-        c.add(passwordField);
-        JLabel label = new JLabel("A label");
-        label.setFont(new Font("Serif", Font.PLAIN, 14));
-        label.setForeground(new Color(0xffffdd));
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(400,600);
-        this.setVisible(true);
         
+        centro = new JPanel();
+        centro.setLayout(new BoxLayout(centro, BoxLayout.PAGE_AXIS));
+        nord = new JPanel();
+        sud = new JPanel();
+
+        emailField = new JTextField();
+        passwordField = new JPasswordField();
+        emailLabel = new JLabel("Email");
+        passwordLabel = new JLabel("Password");
+        loginButton = new JButton("Login");
+
+        loginButton.addActionListener(new LoginButtonListener(this));
+        centro.add(emailLabel);
+        centro.add(emailField);
+        centro.add(passwordLabel);
+        centro.add(passwordField);
+        centro.add(loginButton);
+        
+        nord.add(new JLabel("Effettua il login"));
+        
+        sud.add(new JLabel("Sviluppato con amore dall'Amaro Team"));
+        
+        c.add(nord, BorderLayout.NORTH);
+        c.add(centro, BorderLayout.CENTER);
+        c.add(sud, BorderLayout.SOUTH);
+        
+        pack();
+        
+        dim = Toolkit.getDefaultToolkit().getScreenSize();
+        
+        setLocation((int)((dim.getWidth()-this.getWidth())/2),(int)((dim.getHeight()-this.getHeight())/2));
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+    }
+    
+    public JTextField getEmailField(){
+        return emailField;
+    }
+    
+    public JPasswordField getPasswordField(){
+        return passwordField;
+    }
+
+    public void logged() {
+        setVisible(false);
+        dispose();
     }
 }
