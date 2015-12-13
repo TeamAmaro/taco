88ead384c69fe7f4f349a12552bf4a1793549557
goodnello <grislysquirrel@gmail.com>
2015-12-13 18:24:36 +0100

    Crea il magazziniere attraverso il suo ID

--- it/unisalento/taco/dao/MagazziniereDAO.java
+++ it/unisalento/taco/dao/MagazziniereDAO.java
@@ -8,6 +8,8 @@ package it.unisalento.taco.dao;
 
 import it.unisalento.taco.dbconnections.DBConnection;
 import it.unisalento.taco.model.Magazziniere;
+import it.unisalento.taco.model.Magazzino;
+import it.unisalento.taco.model.Sede;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -22,5 +24,14 @@ public class MagazziniereDAO {
     
     private MagazziniereDAO() {}
         
+    public Magazziniere getMagazziniere(int id) {
+        ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT utenti.id,utenti.nome,utenti.cognome,utenti.email,magazzini.* FROM utenti,magazzinieri,magazzini WHERE utenti.id = id_utente AND utenti.id = " + id + " AND magazzini.id = magazzinieri.id_magazzino");
+        Iterator<String[]> i = result.iterator();
+	String[] riga = i.next();
+        Magazziniere magazziniere = new Magazziniere(id, riga[1], riga[2], riga[3]);
+        Magazzino magazzino = new Magazzino(Integer.parseInt(riga[4]), riga[5], Sede.parseSede(riga[6]));
+        magazziniere.setMagazzino(magazzino);
+        return magazziniere;
+    }
         
 }
