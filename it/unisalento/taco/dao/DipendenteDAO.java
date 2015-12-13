75e2d722d0fcc9eab52837cd943df76da2ee77c9
goodnello <grislysquirrel@gmail.com>
2015-12-13 18:24:08 +0100

    Miglioramenti al codice e adesso assegna un carrello al dipendente

--- it/unisalento/taco/dao/DipendenteDAO.java
+++ it/unisalento/taco/dao/DipendenteDAO.java
@@ -27,9 +27,9 @@ public class DipendenteDAO {
 		while(i.hasNext()) {
 			String[] riga = i.next();
 			int id = Integer.parseInt(riga[0]);
-                        //DA CONTROLLARE!!!
-			Dipendente dipendente = new Dipendente(id, riga[1], riga[2], riga[3], Sede.parseSede(riga[5]));
-			listDipendente.add(dipendente);
+                        //DA SISTEMARE SE SERVE, ALTRIMENTI METODO DA SCARTARE
+			//Dipendente dipendente = new Dipendente(id, riga[1], riga[2], riga[3], Sede.parseSede(riga[5]));
+			//listDipendente.add(dipendente);
 		}
 		
 		return listDipendente;
@@ -40,12 +40,15 @@ public class DipendenteDAO {
 		ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT id,nome,cognome,email,id_progetto,id_sede FROM utenti,dipendenti WHERE id = id_utente AND id = " + id);
 		Iterator<String[]> i = result.iterator();
 		String[] riga = i.next();
-		dip = new Dipendente(id, riga[1], riga[2], riga[3], Sede.SEDE_A);
+                //CREA IL DIPENDENTE CON CARRELLO E PROGETTO NULL
+		dip = new Dipendente(id, riga[1], riga[2], riga[3], Sede.parseSede(riga[4]), null, null);
+                //VEDE SE HA UN PROGETTO DAL DATABASE
 		if(!riga[4].equals("null") || riga[4] != null) {
-			ProgettoDAO progDAO = ProgettoDAO.getInstance();
-			int id_progetto = Integer.parseInt(riga[4]);
-			dip.setProgetto(progDAO.getProgetto(id_progetto));
-		} 
+                        //OTTIENE LE INFORMAZIONI SUL PROGETTO DAL DATABASE E GLIELO ASSOCIA
+			dip.setProgetto(ProgettoDAO.getInstance().getProgetto(Integer.parseInt(riga[4])));
+		}
+                //OTTIENE IL SUO CARRELLO DAL DB E GLIELO ASSOCIA
+                dip.setCarrello(CarrelloDAO.getInstance().getCarrello(dip));
 		return dip; 
 	}
 
