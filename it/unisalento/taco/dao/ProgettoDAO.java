848270cfbd585d3c64ddd14859f70f9397e45151
goodnello <grislysquirrel@gmail.com>
2015-12-14 12:47:34 +0100

    Aggiunto metodo getProgetto(capoprogetto)

--- it/unisalento/taco/dao/ProgettoDAO.java
+++ it/unisalento/taco/dao/ProgettoDAO.java
@@ -7,6 +7,8 @@ import java.util.List;
 import it.unisalento.taco.dbconnections.DBConnection;
 import it.unisalento.taco.model.CapoProgetto;
 import it.unisalento.taco.model.Progetto;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class ProgettoDAO {
 	
@@ -25,7 +27,7 @@ public class ProgettoDAO {
 		ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT * FROM progetti");
 		Iterator<String[]> i = result.iterator();
 		List<Progetto> listProgetto = new ArrayList<>();
-		CapoProgettoDAO capProgDAO = new CapoProgettoDAO();
+		CapoProgettoDAO capProgDAO = CapoProgettoDAO.getInstance();
 		
 		
 		while(i.hasNext()) {
@@ -53,6 +55,19 @@ public class ProgettoDAO {
 		Progetto prog = new Progetto(id, riga[1], capoProg, saldo, budget);
 		return prog;
 	}
+        
+        public Set<Progetto> getProgetto(CapoProgetto capoProgetto) {
+            ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT progetti.* FROM progetti,capiprogetto WHERE id_progetto = progetti.id AND id_utente =" + capoProgetto.getID());
+            Iterator<String[]> i = result.iterator();
+            Set<Progetto> listaProgetti = new LinkedHashSet<>();
+            while(i.hasNext()) {
+                String[] riga = i.next();
+                Progetto progetto = new Progetto(Integer.parseInt(riga[0]), riga[1], capoProgetto, Double.parseDouble(riga[2]), Double.parseDouble(riga[3]));
+                listaProgetti.add(progetto);
+            }
+            
+            return listaProgetti;
+        }
 	
 	public void addProgetto() {
 		
