a7ee5795053b496b3f665f80d76aa2041e204d23
goodnello <grislysquirrel@gmail.com>
2015-12-15 16:28:12 +0100

    Aggiunta getOrdine(hashCode)

--- it/unisalento/taco/dao/OrdineDAO.java
+++ it/unisalento/taco/dao/OrdineDAO.java
@@ -3,9 +3,14 @@ package it.unisalento.taco.dao;
 import it.unisalento.taco.dbconnections.DBConnection;
 import it.unisalento.taco.model.Ordine;
 import it.unisalento.taco.model.Progetto;
+import it.unisalento.taco.model.Dipendente;
+import it.unisalento.taco.model.Magazzino;
+import it.unisalento.taco.model.Prodotto;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class OrdineDAO {
@@ -30,4 +35,23 @@ public class OrdineDAO {
         }
         return listaOrdini;
     }
+    
+    public Ordine getOrdine(int hashCode){
+        ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT * FROM ordini WHERE codice = " + hashCode);
+        Iterator<String[]> i = result.iterator();
+        if(i.hasNext()){
+            String[] riga = i.next();
+            Dipendente dip = DipendenteDAO.getInstance().getDipendente(Integer.parseInt(riga[1]));
+            Progetto prog = ProgettoDAO.getInstance().getProgetto(Integer.parseInt(riga[4]));
+            Magazzino mag = MagazzinoDAO.getInstance().getMagazzino(Integer.parseInt(riga[5]));
+            Prodotto prod = ProdottoDAO.getInstance().getProdotto(Integer.parseInt(riga[6]));
+            int quantProd = Integer.parseInt(riga[7]);
+            Map<Prodotto,Integer> listaProd = new LinkedHashMap<>();
+            long data = Long.parseLong(riga[8]);
+            Ordine ordine = new Ordine(dip, prog, mag, data, listaProd);
+            return ordine;
+        }
+        return null;
+    }
+    
 }
