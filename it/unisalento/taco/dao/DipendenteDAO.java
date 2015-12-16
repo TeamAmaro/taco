f91b8565c9026260f33ff3b0023861ba3e76500f
saucermann <daftpvnk@gmail.com>
2015-12-16 22:48:57 +0100

    Aggiunto getByID

--- it/unisalento/taco/dao/DipendenteDAO.java
+++ it/unisalento/taco/dao/DipendenteDAO.java
@@ -5,60 +5,61 @@ import java.util.Iterator;
 import java.util.List;
 
 import it.unisalento.taco.dbconnections.DBConnection;
+import it.unisalento.taco.exceptions.NoIDMatchException;
+import it.unisalento.taco.model.Carrello;
 import it.unisalento.taco.model.Dipendente;
 import it.unisalento.taco.model.Sede;
 
 public class DipendenteDAO implements DAOInterface{
 	
-	private static DipendenteDAO instance;
-	public static DipendenteDAO getInstance(){
-		if(instance == null)
-			instance = new DipendenteDAO();
-		return instance;
-	}
-	
-	private DipendenteDAO(){};
-	
-	public List<Dipendente> getAllDipendenti() {
-		ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT utenti.*, nome_sede FROM dipendenti,utenti WHERE id_utente = utenti.id");
-		Iterator<String[]> i = result.iterator();
-		List<Dipendente> listDipendente = new ArrayList<>();
-		
-		while(i.hasNext()) {
-			String[] riga = i.next();
-			int id = Integer.parseInt(riga[0]);
-                        //DA SISTEMARE SE SERVE, ALTRIMENTI METODO DA SCARTARE
-			//Dipendente dipendente = new Dipendente(id, riga[1], riga[2], riga[3], Sede.parseSede(riga[5]));
-			//listDipendente.add(dipendente);
-		}
-		
-		return listDipendente;
-	}
+    private static DipendenteDAO instance;
 
-	@Override public Dipendente getByID(int id) {
-		Dipendente dip;
-		ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT id,nome,cognome,email,id_progetto,nome_sede FROM utenti,dipendenti WHERE id = id_utente AND id = " + id);
-		Iterator<String[]> i = result.iterator();
-		String[] riga = i.next();
-                //CREA IL DIPENDENTE CON CARRELLO E PROGETTO NULL
-		dip = new Dipendente(id, riga[1], riga[2], riga[3], Sede.parseSede(riga[5]), null, null);
-                //VEDE SE HA UN PROGETTO DAL DATABASE
-		if(!riga[4].equals("null") || riga[4] != null) {
-                        //OTTIENE LE INFORMAZIONI SUL PROGETTO DAL DATABASE E GLIELO ASSOCIA
-			dip.setProgetto(ProgettoDAO.getInstance().getProgetto(Integer.parseInt(riga[4])));
-		}
-                //OTTIENE IL SUO CARRELLO DAL DB E GLIELO ASSOCIA
-                dip.setCarrello(CarrelloDAO.getInstance().getCarrello(dip));
-		return dip; 
-	}
+    public static DipendenteDAO getInstance(){
+        if(instance == null)
+            instance = new DipendenteDAO();
+        return instance;
+    }
 
-	public void updateDipendente(Dipendente dipendente) {
-		DBConnection.getInstance().updateDB("UPDATE utenti SET nome =" + dipendente.getNome() + ", cognome = " + dipendente.getCognome() + ", email = " + dipendente.getEmail() + " WHERE id = " + dipendente.getID());
-	}
+    private DipendenteDAO(){};
 
-	public void deleteDipendente(Dipendente dipendente) {
-		// DA SCRIVERE QUANDO SERVIRA'
-		
-	}
-	
+    public List<Dipendente> getAllDipendenti() {
+        ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT utenti.*, nome_sede FROM dipendenti,utenti WHERE id_utente = utenti.id");
+        Iterator<String[]> i = result.iterator();
+        List<Dipendente> listaDipendenti = new ArrayList<>();
+
+        while(i.hasNext()) {
+            String[] riga = i.next();
+            int id = Integer.parseInt(riga[0]);
+            try{
+            Dipendente dipendente = getByID(id);
+            listaDipendenti.add(dipendente);
+            }
+            catch(NoIDMatchException e){
+                e.printStackTrace();
+            }
+        }
+        return listaDipendenti;
+}
+
+    @Override public Dipendente getByID(int id) throws NoIDMatchException{
+        ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT id,nome,cognome,email,nome_sede FROM utenti,dipendenti WHERE id = id_utente AND id = " + id);
+        Iterator<String[]> i = result.iterator();
+        if(i.hasNext()){
+            String[] riga = i.next();
+            Carrello carrello = CarrelloDAO.getInstance().getByID(id);
+            Dipendente dip = new Dipendente(id, riga[1], riga[2], riga[3], Sede.parseSede(riga[5]), carrello);
+            return dip; 
+        }
+        else {
+            throw new NoIDMatchException(this);
+        }
+    }
+
+    public void updateDipendente(Dipendente dipendente) {
+        DBConnection.getInstance().updateDB("UPDATE utenti SET nome =" + dipendente.getNome() + ", cognome = " + dipendente.getCognome() + ", email = " + dipendente.getEmail() + " WHERE id = " + dipendente.getID());
+    }
+
+    public void deleteDipendente(Dipendente dipendente) {
+        // DA SCRIVERE QUANDO SERVIRA'
+    }
 }
