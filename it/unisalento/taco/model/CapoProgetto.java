71eee9305a4dbfc647f5c3f9dd8bc90bc1f1c5e5
saucermann <daftpvnk@gmail.com>
2015-12-16 22:54:38 +0100

    Eliminato getAllProgetti ..

--- it/unisalento/taco/model/CapoProgetto.java
+++ it/unisalento/taco/model/CapoProgetto.java
@@ -1,15 +1,7 @@
 package it.unisalento.taco.model;
 
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.Set;
-
 public class CapoProgetto extends Utente {
     public CapoProgetto(int id, String nome, String cognome, String email){
         super(id,nome,cognome,email);
     }
-/*
-    public Set<Progetto> getListaProgetti() {
-        return ;
-    }*/
 }
