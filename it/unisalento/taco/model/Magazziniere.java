1d83873215371bddba70071eb597128f017129f9
saucermann <daftpvnk@gmail.com>
2015-12-16 22:54:59 +0100

    Il costruttore ora accetta magazzino

--- it/unisalento/taco/model/Magazziniere.java
+++ it/unisalento/taco/model/Magazziniere.java
@@ -1,17 +1,14 @@
 package it.unisalento.taco.model;
 public class Magazziniere extends Utente {
 	
-    private Magazzino magazzino;
+    private final Magazzino magazzino;
 	
-    public Magazziniere(int id, String nome,String cognome,String email){
-            super(id,nome,cognome,email);
+    public Magazziniere(int id, String nome,String cognome,String email, Magazzino magazzino){
+        super(id,nome,cognome,email);
+        this.magazzino = magazzino;
     }
-	
-    public void setMagazzino(Magazzino magazzino){
-            this.magazzino = magazzino;
-    }
-	
+
     public Magazzino getMagazzino(){
-            return magazzino;
+        return magazzino;
     }
 }
