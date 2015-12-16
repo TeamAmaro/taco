9a00612075683d927f6f929dd6b98a93d004af4e
saucermann <daftpvnk@gmail.com>
2015-12-16 22:47:42 +0100

    Aggiornato getByProgetto

--- it/unisalento/taco/dao/CapoProgettoDAO.java
+++ it/unisalento/taco/dao/CapoProgettoDAO.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 
 import it.unisalento.taco.dbconnections.DBConnection;
 import it.unisalento.taco.exceptions.NoIDMatchException;
+import it.unisalento.taco.exceptions.NoQueryMatchException;
 import it.unisalento.taco.model.CapoProgetto;
 import it.unisalento.taco.model.Progetto;
 import java.util.Set;
@@ -14,25 +15,32 @@ public class CapoProgettoDAO implements DAOInterface{
     private static CapoProgettoDAO instance;
     
     public static CapoProgettoDAO getInstance(){
-		if(instance == null)
-			instance = new CapoProgettoDAO(){};
-		return instance;
-	}
-	private CapoProgettoDAO (){};
+        if(instance == null)
+            instance = new CapoProgettoDAO(){};
+        return instance;
+    }
+	
+    private CapoProgettoDAO (){};
 
     public ArrayList getAllCapoProgetto() {
-            ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT * FROM capiprogetto");
-            return result;
+        ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT * FROM capiprogetto");
+        //Da scrivere quando serve
+        return result;
     }
 
     //OTTIENE IL CAPOPROGETTO TRAMITE IL PROGETTO
-    public CapoProgetto getByProgetto(Progetto progetto) {
-            ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT id,nome,cognome,email FROM utenti,capiprogetto WHERE id_progetto = " + progetto.getID() + " AND utenti.id = capiprogetto.id_utente");
-            Iterator<String[]> i = result.iterator();
-            String[] riga = i.next();
-            int id = Integer.parseInt(riga[0]);
-            CapoProgetto capoProg = new CapoProgetto(id, riga[1], riga[2], riga[3]);
-            return capoProg;
+    public CapoProgetto getByProgetto(Progetto progetto) throws NoQueryMatchException {
+        ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT id,nome,cognome,email FROM utenti,capiprogetto WHERE id_progetto = " + progetto.getID() + " AND utenti.id = capiprogetto.id_utente");
+        Iterator<String[]> i = result.iterator();
+        if(i.hasNext()){
+        String[] riga = i.next();
+        int id = Integer.parseInt(riga[0]);
+        CapoProgetto capoProg = new CapoProgetto(id, riga[1], riga[2], riga[3]);
+        return capoProg;
+        }
+        else {
+            throw new NoQueryMatchException(this);
+        }
     }
 
     @Override public CapoProgetto getByID(int id) throws NoIDMatchException{
@@ -44,18 +52,7 @@ public class CapoProgettoDAO implements DAOInterface{
             return capoProgetto;
         }
         else {
-            throw new NoIDMatchException("Codice Errore 2: Nessuna corrispondenza nel database");
+            throw new NoIDMatchException(this);
         }
-    }
-
-    /*//OTTIENE IL CAPOPROGETTO TRAMITE L'ID DEL PROGETTO
-    public CapoProgetto getCapoProgetto(int idProgetto) {
-            ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT utenti.* FROM progetti,capiprogetto,utenti WHERE id_progetto = progetti.i AND id_progetto = " + idProgetto + " AND id_utente = utenti.id");
-            Iterator<String[]> i = result.iterator();
-            String[] riga = i.next();
-            int id = Integer.parseInt(riga[0]);
-            CapoProgetto capoProg = new CapoProgetto(id, riga[1], riga[2], riga[3]);
-            return capoProg;
-    }*/
-	
+    }	
 }
