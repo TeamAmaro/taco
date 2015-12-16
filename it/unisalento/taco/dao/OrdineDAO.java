aa8dee3b81c8f1eafaff5bb5d125e2cdd7ed7e83
goodnello <grislysquirrel@gmail.com>
2015-12-16 11:30:00 +0100

    Implementata getListaOrdini(progetto)

--- it/unisalento/taco/dao/OrdineDAO.java
+++ it/unisalento/taco/dao/OrdineDAO.java
@@ -1,11 +1,13 @@
 package it.unisalento.taco.dao;
 
 import it.unisalento.taco.dbconnections.DBConnection;
+import it.unisalento.taco.model.Categoria;
 import it.unisalento.taco.model.Ordine;
 import it.unisalento.taco.model.Progetto;
 import it.unisalento.taco.model.Dipendente;
 import it.unisalento.taco.model.Magazzino;
 import it.unisalento.taco.model.Prodotto;
+import it.unisalento.taco.model.Produttore;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -23,15 +25,18 @@ public class OrdineDAO {
     private OrdineDAO(){}
     
     public Set<Ordine> getListaOrdini(Progetto progetto){
-        //Inizializzo la lista degli ordini, se non ci sono corrispondenze restituisce una lista vuota
         Set<Ordine> listaOrdini = new LinkedHashSet<>();
-        //DA IMPLEMENTARE
         ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT * FROM ordini WHERE id_progetto = " + progetto.getID());
         Iterator<String[]> i = result.iterator();
         while(i.hasNext()) {
             String[] riga = i.next();
-            int idDipendente = Integer.parseInt(riga[1]);
-            //DA IMPLEMENTARE
+            Dipendente dip = DipendenteDAO.getInstance().getDipendente(Integer.parseInt(riga[1]));
+            Progetto prog = ProgettoDAO.getInstance().getProgetto(Integer.parseInt(riga[3]));
+            Magazzino mag = MagazzinoDAO.getInstance().getMagazzino(Integer.parseInt(riga[4]));
+            long data = Long.parseLong(riga[8]);
+            Map<Prodotto,Integer> listaProdotti = getListaProdotti(Integer.parseInt(riga[1]));
+            Ordine ordine = new Ordine(dip,prog,mag,data,listaProdotti);
+            listaOrdini.add(ordine);
         }
         return listaOrdini;
     }
@@ -59,6 +64,20 @@ public class OrdineDAO {
         }
 
         return null;
+    }
+    
+    //SI SUPPONE CHE CI SIANO PRODOTTI NELL'ORDINE E CHE NON POSSA ESISTE UN ORDINE SENZA UNA LISTA DI PRODOTTI
+    public Map<Prodotto,Integer> getListaProdotti(int hashCode) {
+        ArrayList<String[]> result = DBConnection.getInstance().queryDB("SELECT produttori.nome,prodotti.*,quantita FROM ordini,prodotti,produttori WHERE id = produttori.id_prodotto AND id = ordini.id_prodotto AND codice = " + hashCode);
+        Iterator<String[]> i = result.iterator();
+        Map<Prodotto,Integer> listaProdotti = new LinkedHashMap<>();
+        while(i.hasNext()){
+            String[] riga = i.next();
+            Prodotto prodotto = new Prodotto.Builder(Integer.parseInt(riga[1]), riga[2], Double.parseDouble(riga[5]), Produttore.parseProduttore(riga[0])).descrizione(riga[4]).categoria(Categoria.parseCategoria(riga[3])).build();
+            int quantita = Integer.parseInt(riga[6]);
+            listaProdotti.put(prodotto, quantita);
+        }
+        return listaProdotti;
     }
     
 }
