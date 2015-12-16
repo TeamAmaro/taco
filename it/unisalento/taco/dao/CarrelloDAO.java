cbc58d80b0d9f75683dd205f967e7dd4ee43aa07
saucermann <daftpvnk@gmail.com>
2015-12-16 22:48:23 +0100

    Aggiunto getByID (idDipendente)

--- it/unisalento/taco/dao/CarrelloDAO.java
+++ it/unisalento/taco/dao/CarrelloDAO.java
@@ -17,7 +17,7 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class CarrelloDAO {
+public class CarrelloDAO implements DAOInterface{
     
     private static CarrelloDAO instance;
     
@@ -30,13 +30,15 @@ public class CarrelloDAO {
     private CarrelloDAO(){}
 
     //RESTITUISCE SOLO LA LISTA DEI PRODOTTI NEL CARRELLO DEL DIPENDENTE
-    public Map<Prodotto,Integer> getListaProdotti(Dipendente dipendente){
+    
+    public Map<Prodotto,Integer> getListaProdotti(int idDip){
         
-        String uberQuery = "SELECT produttori.nome,prodotti.*,carrelli.quantita FROM prodotti,carrelli,produttori,dipendenti WHERE carrelli.id_prodotto = prodotti.id AND dipendenti.id_utente = " + dipendente.getID() + " AND produttori.id_prodotto = prodotti.id";
+        String uberQuery = "SELECT produttori.nome,prodotti.*,carrelli.quantita FROM prodotti,carrelli,produttori,dipendenti WHERE carrelli.id_prodotto = prodotti.id AND dipendenti.id_utente = " + idDip + " AND produttori.id_prodotto = prodotti.id";
         ArrayList<String[]> result = DBConnection.getInstance().queryDB(uberQuery);
         Iterator<String[]> i = result.iterator();
         Map<Prodotto,Integer> listaProdotti = new LinkedHashMap<>();
-        
+        //Se il contenuto della query è nullo, lo è anche il carrello
+        //Non si restituisce null ma un carrello vuoto.
         while(i.hasNext()) {
             String[] riga = i.next();
             Prodotto prodotto = new Prodotto.Builder(Integer.parseInt(riga[1]), riga[2], Double.parseDouble(riga[5]), Produttore.parseProduttore(riga[0])).categoria(Categoria.parseCategoria(riga[3])).descrizione(riga[4]).build();
@@ -51,7 +53,16 @@ public class CarrelloDAO {
     
     //RESTITUISCE IL CARRELLO
     public Carrello getCarrello(Dipendente dipendente) {
-        Carrello carrello = new Carrello(getListaProdotti(dipendente));
+        Carrello carrello = new Carrello(getListaProdotti(dipendente.getID()));
+        return carrello;
+    }
+    
+    //L'id che accetta come parametro è quello del dipendente,
+    //infatti, per ogni dipendente c'è un solo ed unico carrello.
+    //Non ha senso per questa funzione lanciare un'eccezione 
+    //Perché se il dipendente esiste, esiste anche il suo carrello.
+    @Override public Carrello getByID(int idDip){
+        Carrello carrello = new Carrello(getListaProdotti(idDip));
         return carrello;
     }
     
