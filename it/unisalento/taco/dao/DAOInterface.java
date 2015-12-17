ca630e3885c11fc94fae0b412e5cbe316afdf8e8
goodnello <grislysquirrel@gmail.com>
2015-12-17 14:10:45 +0100

    Metodo delete implementato da tutti i DAO, metodo update da vedere

--- it/unisalento/taco/dao/DAOInterface.java
+++ it/unisalento/taco/dao/DAOInterface.java
@@ -6,7 +6,10 @@
 package it.unisalento.taco.dao;
 
 import it.unisalento.taco.exceptions.NoIDMatchException;
+import it.unisalento.taco.model.IdentificabileID;
 
 public interface DAOInterface {
     public Object getByID(int id) throws NoIDMatchException;
+    //public void update(IdentificabileID obj);
+    public void delete(IdentificabileID obj);
 }
