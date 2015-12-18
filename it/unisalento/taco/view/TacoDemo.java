4e4ad0e42804e7b149f5164ad04a511efc3814a5
saucermann <daftpvnk@gmail.com>
2015-12-18 12:31:38 +0100

    Test riusciti (I valori modificati però sono temporanei se non è effettuato un push al db)

--- it/unisalento/taco/view/TacoDemo.java
+++ it/unisalento/taco/view/TacoDemo.java
@@ -5,7 +5,9 @@ import it.unisalento.taco.dbconnections.*;
 import it.unisalento.taco.dao.*;
 import java.util.Map;
 import it.unisalento.taco.business.GeneratoreOrdini;
+import it.unisalento.taco.exceptions.InsufficientFundException;
 import it.unisalento.taco.exceptions.NoIDMatchException;
+import it.unisalento.taco.exceptions.NoQueryMatchException;
 
 public class TacoDemo {
 
@@ -14,14 +16,15 @@ public class TacoDemo {
                 try{
                     Dipendente guga = DipendenteDAO.getInstance().getByID(1);
                     Map<Magazzino,Map<Prodotto,Integer>> mappa = GeneratoreOrdini.getInstance().magazzinoPerProdotto(guga);
+                    System.out.println(Carrello.getByID(guga.getID()));
+                    System.out.println(mappa);
+                    Progetto progetto = Progetto.getProgetto(guga);
+                    System.out.println(progetto);
                     GeneratoreOrdini.getInstance().generaOrdini(guga, mappa);
-                    Ordine dao = OrdineDAO.getInstance().getByID(-1099431227);
-                    System.out.println("CODICE INSERITO: -1099431227 / CODICE HASH OTTENUTO : " + dao.hashCode());
-                    System.out.println("Se sono uguali il sistema funziona");
-                    System.out.println("Se non sono uguali controlla il database!");
+                    System.out.println(progetto);
                 }
-                catch(NoIDMatchException e) {
-                    e.printStackTrace();
+                catch(NoIDMatchException | InsufficientFundException | NoQueryMatchException e) {
+                    System.err.println(e.getMessage());
                 }
 	}
 }
