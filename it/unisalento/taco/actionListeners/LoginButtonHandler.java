71a26ee6bd0a207a02b129d65cbacff797251278
saucermann <daftpvnk@gmail.com>
2015-12-21 12:45:11 +0100

    Esistenza di questa classe da valutare in seguito

---
+++ it/unisalento/taco/actionListeners/LoginButtonHandler.java
@@ -0,0 +1,12 @@
+package it.unisalento.taco.actionListeners;
+
+import it.unisalento.taco.view.Main;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.paint.Color;
+
+public class LoginButtonHandler implements EventHandler<ActionEvent>{
+    @Override public void handle(ActionEvent e) {
+            
+        }
+}
