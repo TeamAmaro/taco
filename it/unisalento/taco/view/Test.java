4e1c35e89923db6de83b3689af5b76d9b4ca1c45
goodnello <grislysquirrel@gmail.com>
2015-12-24 13:46:03 +0100

    Test vari

--- it/unisalento/taco/view/Test.java
+++ it/unisalento/taco/view/Test.java
@@ -9,17 +9,21 @@ package it.unisalento.taco.view;
 import it.unisalento.taco.business.AdminDelegate;
 import it.unisalento.taco.dao.MagazzinoDAO;
 import it.unisalento.taco.dao.MagazziniereDAO;
+import it.unisalento.taco.exceptions.NoQueryMatchException;
+import it.unisalento.taco.model.CapoProgetto;
 import it.unisalento.taco.model.Magazziniere;
 import it.unisalento.taco.model.Magazzino;
+import it.unisalento.taco.model.Progetto;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 
 public class Test {
 
-    public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException{
-        Magazzino magazzino = MagazzinoDAO.getInstance().getMagazzino(3);
-        Magazziniere magazziniere = MagazziniereDAO.getInstance().getMagazziniere("paolino.paperino@topolino.it");
-        AdminDelegate.getInstance().setMagazzino(magazziniere, magazzino);
+    public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException, NoQueryMatchException{
+        CapoProgetto capo = CapoProgetto.getCapoProgetto("alfonso.verde@green.ve");
+        AdminDelegate.getInstance().creaProgetto("Progetto Zero", capo, 20000);
+        Progetto prog = Progetto.getProgetto("Progetto Zero");
+        AdminDelegate.getInstance().setCapoProgetto(prog, capo);
     }
     
     
