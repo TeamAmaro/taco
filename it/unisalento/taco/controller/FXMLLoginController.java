e1b2d216cef7738115d89e2b5921026edd6f0698
saucermann <daftpvnk@gmail.com>
2016-01-03 13:39:12 +0100

    Effettua switch tra tipo di client

--- it/unisalento/taco/controller/FXMLLoginController.java
+++ it/unisalento/taco/controller/FXMLLoginController.java
@@ -1,5 +1,9 @@
 package it.unisalento.taco.controller;
 
+import it.unisalento.taco.model.Admin;
+import it.unisalento.taco.model.CapoProgetto;
+import it.unisalento.taco.model.Dipendente;
+import it.unisalento.taco.model.Magazziniere;
 import it.unisalento.taco.view.Main;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -33,10 +37,20 @@ public class FXMLLoginController extends AnchorPane implements Initializable {
     }
     
     public void effettuaLogin(ActionEvent event) {
-        if (!application.login(emailField.getText(), passwordField.getText())){
+        if (!application.setClient(emailField.getText(), passwordField.getText())){
             errorMessage.setText("Combinazione Email/Password non corretta!");
         }
-        else 
+        else {
             errorMessage.setText("Benvenuto a TACO " + emailField.getText());
+            
+            if(application.getClient() instanceof Dipendente)
+                application.dipendenteView();
+            else if(application.getClient() instanceof CapoProgetto)
+                application.capoProgettoView();
+            else if(application.getClient() instanceof Magazziniere)
+                application.magazziniereView();
+            else if(application.getClient() instanceof Admin)
+               ;//application.adminView();
+        }
     }
 }
