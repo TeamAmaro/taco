4894aec18c30bc356ce64f0a585b3bdecb811bbf
goodnello <grislysquirrel@gmail.com>
2016-01-03 11:36:09 +0100

    Modifiche minori

--- it/unisalento/taco/business/DipendenteDelegate.java
+++ it/unisalento/taco/business/DipendenteDelegate.java
@@ -4,25 +4,16 @@ import it.unisalento.taco.model.Dipendente;
 import it.unisalento.taco.model.Prodotto;
 
 public class DipendenteDelegate {
-	private static DipendenteDelegate instance ;
 	
-	public static DipendenteDelegate getInstance(){
-		if(instance == null)
-                    instance = new DipendenteDelegate();
-		return instance;
-	}
-	private DipendenteDelegate(){};
-	
-	public void aggiungiProdotto(Dipendente dipendente, Prodotto prodotto, int quantita){
-		
-	}
-	
-	public void rimuoviProdotto(Dipendente dipendente, Prodotto prodotto, int quantita){
+    private static DipendenteDelegate instance ;
 
-        }
-	
-	public void acquista(Dipendente dipendente){
-		//Generatore Ordini
-	}
-	
+    public static DipendenteDelegate getInstance(){
+        if(instance == null)
+            instance = new DipendenteDelegate();
+        return instance;
+    }
+    private DipendenteDelegate(){};
+
+    
+    
 }
