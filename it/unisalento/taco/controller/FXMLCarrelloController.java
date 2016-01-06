ab2dfbbf76cb13ece36e25564c754dc69b2ae5da
saucermann <daftpvnk@gmail.com>
2016-01-06 22:08:51 +0100

    tornare indietro, effettuare ordine, rimuovere prodotto

--- it/unisalento/taco/controller/FXMLCarrelloController.java
+++ it/unisalento/taco/controller/FXMLCarrelloController.java
@@ -10,11 +10,16 @@ import it.unisalento.taco.exceptions.NoIDMatchException;
 import it.unisalento.taco.exceptions.NoQueryMatchException;
 import it.unisalento.taco.model.Carrello;
 import it.unisalento.taco.model.Dipendente;
+import it.unisalento.taco.model.Ordine;
 import it.unisalento.taco.model.Prodotto;
 import it.unisalento.taco.view.Main;
 import java.net.URL;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -27,20 +32,42 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 public class FXMLCarrelloController implements Initializable {
 
     @FXML GridPane content;
     @FXML ImageView leftLogo;
     @FXML Label logout;
+    @FXML HBox topLeft;
+    @FXML Label totale;
+    @FXML Button ordinaButton;
     
     private Main application;
     private Carrello carrello;
     private DipendenteDelegate delegate = DipendenteDelegate.getInstance();
 
+    @FXML ImageView iv;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        //TO-DO
+        
+        iv = new ImageView(new Image("it/unisalento/taco/view/img/back.jpg"));
+        iv.setFitHeight(50.0);
+        iv.setPreserveRatio(true);
+        
+        topLeft.getChildren().add(0, iv);
+
+        FadeTransition ft = new FadeTransition(Duration.millis(1000));
+        ft.setFromValue(0.3f);
+        ft.setToValue(1.0f);
+        TranslateTransition tt = new TranslateTransition(Duration.millis(1000));
+        tt.setFromX(-100f);
+        tt.setToX(0);
+        
+        ParallelTransition pt = new ParallelTransition(iv, ft, tt);
+        pt.play();
+        
     }    
     
     public void setApplication(Main application){
@@ -48,27 +75,39 @@ public class FXMLCarrelloController implements Initializable {
     }
     
     public void initData(){
+        
         int i = 2;
+        
         try {
             carrello = delegate.getCarrello((Dipendente) application.getUtente());
         } catch (NoIDMatchException e){
             System.err.println(e.getMessage());
         }
+        
         Map<Prodotto, Integer> listaProdotti = carrello.getListaProdotti();
-        System.out.println(carrello);
+        
         if(listaProdotti.isEmpty()){
+            
             content.getChildren().clear();
+            
             Label message = new Label("Il tuo carrello è vuoto!");
             Label message2 = new Label("Visita il catalogo per aggiungere prodotti al carrello.");
+            
             message2.setOnMouseClicked(new EventHandler<MouseEvent>(){
                 @Override public void handle(MouseEvent arg0) {
                     application.dipendenteView();
                 }
             });
+            
             content.add(message, 0, 0);
             content.add(message2, 0, 1);
+            
+            ordinaButton.setDisable(true);
         }
         else{
+            
+            totale.setText(Double.toString(carrello.getTotale()) + "€");
+            
             for(Map.Entry<Prodotto,Integer> lp : listaProdotti.entrySet()) {
                 
                 final Prodotto prodotto = lp.getKey();
@@ -120,6 +159,7 @@ public class FXMLCarrelloController implements Initializable {
                         delegate.removeProdotto(carrello, prodotto);
                         rimuovi.setDisable(true);
                         rimuovi.setText("Rimosso!");
+                        totale.setText(Double.toString(carrello.getTotale()) + "€");
                     }
                 });
 
@@ -134,15 +174,39 @@ public class FXMLCarrelloController implements Initializable {
             }
         }
         
+        
+        
         leftLogo.setOnMouseClicked(new EventHandler<MouseEvent>(){
-                @Override public void handle(MouseEvent arg0) {
-                    application.dipendenteView();
-                }
+            @Override public void handle(MouseEvent arg0) {
+                application.dipendenteView();
+            }
         });
         
         logout.setOnMouseClicked(new EventHandler<MouseEvent>(){
             @Override public void handle(MouseEvent arg0) {
                 application.logout();
+            }
+        });
+        
+        iv.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override public void handle(MouseEvent arg0) {
+                application.lastView();
+            }
+        });
+        
+        ordinaButton.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override public void handle(MouseEvent arg0) {
+                try{
+                    Set<Ordine> listaOrdini = delegate.generaOrdini((Dipendente) application.getUtente());
+                    for(Ordine o : listaOrdini)
+                        System.out.println(o);
+                } catch (NoIDMatchException e){
+                    System.err.println(e.getMessage());
+                } catch (NoQueryMatchException e){
+                    System.err.println("WUT");
+                } finally {
+                    //DO SOMETHING!
+                }
             }
         });
     }
