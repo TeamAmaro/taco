b15b610be7d7bf7b4806cca6788abcb26a2a731c
saucermann <daftpvnk@gmail.com>
2016-01-21 13:00:38 +0100

    Aggiunte funzioni di delegate. Commentate le cose strane

--- it/unisalento/taco/business/MagazziniereDelegate.java
+++ it/unisalento/taco/business/MagazziniereDelegate.java
@@ -1,9 +1,14 @@
 package it.unisalento.taco.business;
 
+import it.unisalento.taco.exceptions.NoIDMatchException;
+import it.unisalento.taco.exceptions.NoQueryMatchException;
+import it.unisalento.taco.model.Magazziniere;
 import java.util.Map;
-
 import it.unisalento.taco.model.Magazzino;
+import it.unisalento.taco.model.Ordine;
 import it.unisalento.taco.model.Prodotto;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class MagazziniereDelegate {
 	
@@ -14,6 +19,7 @@ public class MagazziniereDelegate {
                 instance = new MagazziniereDelegate();
         return instance;
     }
+    
     private MagazziniereDelegate(){};
 
     public void rifornisciProdotto(Magazzino magazzino, Prodotto prodotto, int quantita){
@@ -23,15 +29,23 @@ public class MagazziniereDelegate {
     public void spedisciProdotto(Magazzino magazzino, Prodotto prodotto, int quantita){
         magazzino.removeProdotto(prodotto, quantita);
     };
-
-    
+    //........ಠ_ಠ
     public Map<Prodotto,Integer> chiediInventario(Magazzino magazzino){
         return magazzino.getInventario();
     }
-
+    //.......ಠ_ಠ
     public int chiediProdotto(Magazzino magazzino, Prodotto prodotto, int quantita){
         return magazzino.cercaProdotto(prodotto, quantita);
     }
-        
-	
+
+    public Magazzino getMagazzino(Magazziniere magazziniere) throws NoQueryMatchException{
+        return Magazzino.getMagazzino(magazziniere);
+    }
+    
+    public Set<Ordine> chiediOrdini(Magazziniere magazziniere) throws NoQueryMatchException, NoIDMatchException{
+        Set<Ordine> listaOrdini = new LinkedHashSet<>();
+        Magazzino magazzino = Magazzino.getMagazzino(magazziniere);
+        listaOrdini = Ordine.getListaOrdini(magazzino);
+        return listaOrdini;
+    }
 }
