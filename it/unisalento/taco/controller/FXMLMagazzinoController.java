f89658aa2e1b933239febcb2c31e8d8cb01e9c01
saucermann <daftpvnk@gmail.com>
2016-01-21 13:02:29 +0100

    Prima versione del controller del magazzino. Quasi completo

--- it/unisalento/taco/controller/FXMLMagazzinoController.java
+++ it/unisalento/taco/controller/FXMLMagazzinoController.java
@@ -1,27 +1,100 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.unisalento.taco.controller;
 
+import it.unisalento.taco.business.MagazziniereDelegate;
+import it.unisalento.taco.exceptions.NoIDMatchException;
+import it.unisalento.taco.exceptions.NoQueryMatchException;
+import it.unisalento.taco.model.Magazziniere;
+import it.unisalento.taco.model.Magazzino;
+import it.unisalento.taco.model.Ordine;
+import it.unisalento.taco.model.Prodotto;
+import it.unisalento.taco.view.Main;
 import java.net.URL;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
-/**
- * FXML Controller class
- *
- * @author tomma
- */
 public class FXMLMagazzinoController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
+    
+    @FXML AnchorPane anchorPane;
+    
+    @FXML Label nomeClient;
+    @FXML Label spedizioni;
+    @FXML Label inventario;
+    @FXML Label nomeMagazzino;
+    @FXML Label notificaSpedizione;
+    @FXML Label notificaInventario;
+    @FXML Label logout;
+    
+    private Main application;
+    private MagazziniereDelegate delegate = MagazziniereDelegate.getInstance();
+    private Magazzino magazzino;
+    
+    public void setApplication(Main application){
+        this.application = application;
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
     
+    public void initData(){
+        initInfo();
+        initMenu();
+    }
+    
+    private void initInfo(){
+        nomeClient.setText(application.getUtente().getNome() + " " + application.getUtente().getCognome());
+        try{
+            magazzino = MagazziniereDelegate.getInstance().getMagazzino((Magazziniere) application.getUtente());
+            nomeMagazzino.setText(magazzino.getNome());
+            Set<Ordine> listaOrdini = delegate.chiediOrdini((Magazziniere) application.getUtente());
+            notificaSpedizione.setText("Hai " + listaOrdini.size() + " ordini da spedire.");
+            boolean check = false;
+            StringBuilder sb = new StringBuilder();
+            sb.append("Esaurimento scorte dei seguenti prodotti:").append(System.lineSeparator());
+            for(Map.Entry<Prodotto,Integer> k : magazzino.getInventario().entrySet()){
+                if(k.getValue() < 10){
+                    check = true;
+                    sb.append(k.getKey().getNome() + " : " + k.getValue() + " in magazzino." + System.lineSeparator());
+                }    
+            }
+            if(check == false)
+                notificaInventario.setText("Nessun prodotto in esaurimento scorte.");
+            else
+                notificaInventario.setText(sb.toString());
+        }catch(NoQueryMatchException e){
+            //DA DEFINIRE!!
+        } catch (NoIDMatchException ex) {
+            Logger.getLogger(FXMLMagazzinoController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void initMenu(){
+        logout.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override public void handle(MouseEvent arg0) {
+                application.logout();
+            }
+        });
+        
+        spedizioni.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override public void handle(MouseEvent arg0) {
+                application.getSpedizione(magazzino);
+            }
+        });
+        
+        inventario.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override public void handle(MouseEvent arg0) {
+                application.getInventario(magazzino);
+            }
+        });
+    }
 }
