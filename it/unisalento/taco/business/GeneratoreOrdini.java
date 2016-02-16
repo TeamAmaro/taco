0fbb21b0a8e26bcfa07f5e3ccd57d8d81ac5aeaf
saucermann <daftpvnk@gmail.com>
2016-02-16 11:24:21 +0100

    Il miglior metodo di generazione ordini dell'ultimo secolo

--- it/unisalento/taco/business/GeneratoreOrdini.java
+++ it/unisalento/taco/business/GeneratoreOrdini.java
@@ -13,14 +13,12 @@ import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class GeneratoreOrdini {
     
     private static GeneratoreOrdini instance;
     
-    private static GeneratoreOrdini getInstance(){
+    public static GeneratoreOrdini getInstance(){
         if(instance == null)
             instance = new GeneratoreOrdini();
         
@@ -29,70 +27,75 @@ public class GeneratoreOrdini {
     
     private GeneratoreOrdini(){}
     
-    public Set<Ordine> generaOrdini(Dipendente dipendente){
+    public Set<Ordine> generaOrdini(Dipendente dipendente) throws NoQueryMatchException, NoIDMatchException{
         Set<Ordine> listaOrdini = new LinkedHashSet<>();
         try {
             Map<Prodotto,Integer> listaProdotti = Carrello.getCarrello(dipendente).getListaProdotti();
             Magazzino magDipendente = Magazzino.getMagazzino(dipendente.getSede());
             
             Map<Magazzino, Map<Prodotto,Integer>> magPerProdQuant = new LinkedHashMap<>();
-            
+            Map<Prodotto,Integer> prodQuantMagDip = new LinkedHashMap<>();
+            magPerProdQuant.put(magDipendente, prodQuantMagDip);
             
             for(Map.Entry<Prodotto,Integer> prodQuantEntry : listaProdotti.entrySet()){
                 
                 Prodotto prodRichiesto = prodQuantEntry.getKey();
                 int quantMagDip = magDipendente.getInventario().get(prodRichiesto);
                 int quantRichiesta = prodQuantEntry.getValue();
-                Map<Prodotto,Integer> prodQuantMagDip = new LinkedHashMap<>();
-                
+                                
                 if(quantMagDip < quantRichiesta){
-                    
+                                        
                     quantRichiesta -= quantMagDip;
+                    if(quantMagDip != 0)
+                        prodQuantMagDip.put(prodRichiesto, quantMagDip);
                     
-                    
-                    prodQuantMagDip.put(prodRichiesto, quantMagDip);
-                    magPerProdQuant.put(magDipendente, prodQuantMagDip);
-                        
                     //Chiedi agli altri di soddisfare la richiesta
-                    
                     Set<Magazzino> listaMag = Magazzino.cercaProdotto(prodRichiesto);
-                    
                     listaMag.remove(magDipendente);
                                         
                     for(Magazzino magExt : listaMag){
-                        
                         int quantMagExt = magExt.getInventario().get(prodRichiesto);
+                        
+                        if(quantMagExt <= 0)
+                            continue;
+                        
                         if(quantMagExt < quantRichiesta){
                             quantRichiesta -= quantMagExt;
-                            Map<Prodotto,Integer> prodQuantMagExt = new LinkedHashMap<>();
-                            prodQuantMagExt.put(prodRichiesto, quantMagExt);
-                            magPerProdQuant.put(magExt, prodQuantMagExt);
+                            
+                            if(magPerProdQuant.containsKey(magExt))
+                                magPerProdQuant.get(magExt).put(prodRichiesto, quantMagExt);
+                            else {
+                                Map<Prodotto,Integer> prodQuantMagExt = new LinkedHashMap<>();
+                                prodQuantMagExt.put(prodRichiesto, quantMagExt);
+                                magPerProdQuant.put(magExt, prodQuantMagExt);
+                            }
                         }else {
-                            Map<Prodotto,Integer> prodQuantMagExt = new LinkedHashMap<>();
-                            prodQuantMagExt.put(prodRichiesto, quantRichiesta);
-                            magPerProdQuant.put(magExt, prodQuantMagExt);
+                            if(magPerProdQuant.containsKey(magExt))
+                                magPerProdQuant.get(magExt).put(prodRichiesto, quantRichiesta);
+                            else {
+                                Map<Prodotto,Integer> prodQuantMagExt = new LinkedHashMap<>();
+                                prodQuantMagExt.put(prodRichiesto, quantRichiesta);
+                                magPerProdQuant.put(magExt, prodQuantMagExt);
+                            }
                             break;
                         }
 			if(quantRichiesta <= 0)
-				break;
+                            break;
                     }
                 } else {
                     //Soddisfa la richiesta e passa al prossimo prodotto
                     prodQuantMagDip.put(prodRichiesto, quantRichiesta);
-                    magPerProdQuant.put(magDipendente, prodQuantMagDip);
                 }
             }
-                
             Date date = new Date();
             long unixTime = date.getTime();
-
-            for(Map.Entry<Magazzino,Map<Prodotto,Integer>> mag : magPerProd.entrySet()){
+            for(Map.Entry<Magazzino,Map<Prodotto,Integer>> mag : magPerProdQuant.entrySet()){
                 Ordine ordine = new Ordine(dipendente, Progetto.getProgetto(dipendente), mag.getKey(), unixTime, mag.getValue());
                 listaOrdini.add(ordine);
             }
-        } catch (NoIDMatchException | NoQueryMatchException ex) {
-            Logger.getLogger(GeneratoreOrdini.class.getName()).log(Level.SEVERE, null, ex);
+            return listaOrdini;
+        } catch (NoQueryMatchException ex) {
+            throw ex;
         }
-        return listaOrdini;
     }
 }
