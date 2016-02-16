dcc61010a2ef1cb0711b48e82e3aaa07af63afc7
saucermann <daftpvnk@gmail.com>
2016-02-16 18:09:41 +0100

    Migliorata quella funzione inguardabile

--- it/unisalento/taco/business/DocumentManager.java
+++ it/unisalento/taco/business/DocumentManager.java
@@ -5,7 +5,6 @@ import it.unisalento.taco.model.Prodotto;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.math.RoundingMode;
 import java.text.NumberFormat;
 import java.util.Locale;
@@ -15,7 +14,6 @@ import javax.imageio.ImageIO;
 import org.apache.pdfbox.exceptions.COSVisitorException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.common.PDStream;
 import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
@@ -35,131 +33,89 @@ public class DocumentManager {
     
     private DocumentManager(){}
     
-    public void generaRicevuta(Set<Ordine> listaOrdini, File file) throws IOException, COSVisitorException {	
-        try {
+    public void generaRicevuta(Set<Ordine> listaOrdini, File file) throws IOException, COSVisitorException{
+        PDDocument document = new PDDocument();
+        PDFont font = PDType1Font.HELVETICA;
+        
+        BufferedImage logoBuffer = ImageIO.read(new File("src/it/unisalento/taco/view/img/logo2.png"));
+        PDXObjectImage logo = new PDPixelMap(document, logoBuffer);
             
-            PDDocument document = new PDDocument();
-            PDFont font = PDType1Font.HELVETICA;
+        for(Ordine o : listaOrdini){
             
-            BufferedImage logoBuffer = ImageIO.read(new File("src/it/unisalento/taco/view/img/logo2.png"));
-            PDXObjectImage logo = new PDPixelMap(document, logoBuffer);
-            
-            for(Ordine o : listaOrdini){
-                
-                int i = 730;
-                PDPage page = new PDPage(PDPage.PAGE_SIZE_A4);
-                document.addPage(page);
-                
-                try (PDPageContentStream cos = new PDPageContentStream(document, page)) {
-                    
-                    cos.drawImage(logo, 130, i);
-                    cos.setFont(font, 12);
-                    
-                    i-=40;
-                    
-                    cos.beginText();
-                    cos.moveTextPositionByAmount(50, i);
-                    cos.drawString("Ricevuta ordine di " + o.getDipendente().getNome() + " " + o.getDipendente().getCognome());
-                    cos.endText();
-                    
-                    i-= 20;
-                    
-                    cos.beginText();
-                    cos.moveTextPositionByAmount(50, i);
-                    cos.drawString("CODICE: " + o.getCodice());
-                    cos.endText();
-                    
-                    i-= 20;
-                    
-                    cos.beginText();
-                    cos.moveTextPositionByAmount(50, i);
-                    cos.drawString("DIPENDENTE: " + o.getDipendente().getNome() + " " + o.getDipendente().getCognome());
-                    cos.endText();
-                    
-                    i-= 20;
-                    
-                    cos.beginText();
-                    cos.moveTextPositionByAmount(50, i);
-                    cos.drawString("PROGETTO: " + o.getProgetto().getNome() + " - " + o.getProgetto().getCapoProgetto().getNome() +" " + o.getProgetto().getCapoProgetto().getCognome());
-                    cos.endText();
-                    
-                    i-= 20;
-                    
-                    cos.beginText();
-                    cos.moveTextPositionByAmount(50, i);
-                    cos.drawString("DATA: " + o.getReadableData());
-                    cos.endText();
-                    
-                    i-= 20;
-                    
-                    cos.beginText();
-                    cos.moveTextPositionByAmount(50, i);
-                    cos.drawString("LISTA PRODOTTI");
-                    cos.endText();
-                    
-                    i-= 20;
-                    
-                    for(Map.Entry<Prodotto,Integer> e : o.getListaProdotti().entrySet()){
+            int offsetY = 730;
+            PDPage page = new PDPage(PDPage.PAGE_SIZE_A4);
+            document.addPage(page);
+            try(PDPageContentStream cos = new PDPageContentStream(document,page)){
+                cos.drawImage(logo, 130, offsetY);
+                cos.setFont(font, 12);
+                offsetY-=40;
+
+                String[] parteUno = {"Ricevuta ordine di " + o.getDipendente().getNome() + " " + o.getDipendente().getCognome(), 
+                                    "CODICE: " + o.getCodice(),
+                                    "DIPENDENTE: " + o.getDipendente().getNome() + " " + o.getDipendente().getCognome(),
+                                    "PROGETTO: " + o.getProgetto().getNome() + " - " + o.getProgetto().getCapoProgetto().getNome() +" " + o.getProgetto().getCapoProgetto().getCognome(),
+                                    "DATA: " + o.getReadableData(),
+                                    "LISTA PRODOTTI"};
+
+                String [] listaProdotti = new String[o.getListaProdotti().keySet().size()];
+
+                int k = 0;
+                for(Map.Entry<Prodotto,Integer> e : o.getListaProdotti().entrySet()){
+                   listaProdotti[k++] = "\"" + e.getKey().getNome() + "\" " + e.getKey().getFormatPrezzo().replace('€', ' ') + " euro"  +  " x " + e.getValue();
+                }
+
+                double totale;
+                NumberFormat formatoEuro = NumberFormat.getCurrencyInstance(Locale.ITALY);
+                formatoEuro.setMinimumFractionDigits( 2 );
+                formatoEuro.setMaximumFractionDigits( 2 );
+                formatoEuro.setRoundingMode(RoundingMode.HALF_EVEN);
+                totale = o.getTotale() + o.getSpesaSpedizione();
+
+                String [] parteDue = {"MAGAZZINO: " + o.getMagazzino().getNome() + " (" + o.getMagazzino().getSede().nome() +  ")",
+                                      "SUBTOTALE: " + o.getFormatTotale().replace('€', ' ') + " euro",
+                                      "COSTO SPEDIZIONE: " + o.getFormatSpesaSpedizione().replace('€', ' ') + " euro",
+                                      "TOTALE: " + formatoEuro.format(totale).replace('€', ' ') + " euro"};
+
+                for (int i = 0; i < parteUno.length; i++){
+                    try {
                         cos.beginText();
-                        cos.moveTextPositionByAmount(50, i);
-                        cos.drawString("\"" + e.getKey().getNome() + "\" " + e.getKey().getFormatPrezzo().replace('€', ' ') + " euro"  +  " x " + e.getValue());
+                        cos.moveTextPositionByAmount(50, offsetY);
+                        cos.drawString(parteUno[i]);
                         cos.endText();
-                        i -= 20;
-                        /*
-                        //Se si raggiunge il fondo della pagina
-                        if(i <= 80)
-                        {
-                            cos.close();
-                            i = 730;
-                            page = new PDPage(PDPage.PAGE_SIZE_A4);
-                            document.addPage(page);
-
-                            
-                            cos.drawImage(logo, 130, i);
-                            cos.setFont(font, 12);
-                            i-=40;
-                        }*/
+                        offsetY -= 20;
+                    } catch (IOException ex) {
+                       throw ex;
                     }
-                      
-                    cos.beginText();
-                    cos.moveTextPositionByAmount(50, i);
-                    cos.drawString("MAGAZZINO: " + o.getMagazzino().getNome() + " (" + o.getMagazzino().getSede().nome() +  ")");
-                    cos.endText();
-                    i-= 20;
-                    
-                    cos.beginText();
-                    cos.moveTextPositionByAmount(50, i);
-                    cos.drawString("TOTALE PRODOTTI: " + o.getFormatTotale().replace('€', ' ') + " euro");
-                    cos.endText();
-                    i-= 20;
-                    
-                    cos.beginText();
-                    cos.moveTextPositionByAmount(50, i);
-                    cos.drawString("COSTO SPEDIZIONE: " + o.getFormatSpesaSpedizione().replace('€', ' ') + " euro");
-                    cos.endText();
-                    i-= 20;
-                    
-                    double totale;
-                    NumberFormat formatoEuro = NumberFormat.getCurrencyInstance(Locale.ITALY);
-                    formatoEuro.setMinimumFractionDigits( 2 );
-                    formatoEuro.setMaximumFractionDigits( 2 );
-                    formatoEuro.setRoundingMode(RoundingMode.HALF_EVEN);
+                }
 
-                    totale = o.getTotale() + o.getSpesaSpedizione();
-                                
-                    cos.beginText();
-                    cos.moveTextPositionByAmount(50, i);
-                    cos.drawString("TOTALE: " + formatoEuro.format(totale).replace('€', ' ') + " euro");
-                    cos.endText();
-                    cos.close();
+                for (int i = 0; i < listaProdotti.length; i++){
+                    try {
+                        cos.beginText();
+                        cos.moveTextPositionByAmount(50, offsetY);
+                        cos.drawString(listaProdotti[i]);
+                        cos.endText();
+                        offsetY -= 20;
+                    } catch (IOException ex) {
+                       throw ex;
+                    }
+                }
+
+                for(int i = 0; i < parteDue.length; i++){
+                    try {
+                        cos.beginText();
+                        cos.moveTextPositionByAmount(50, offsetY);
+                        cos.drawString(parteDue[i]);
+                        cos.endText();
+                        offsetY-=20;
+                    } catch (IOException ex) {
+                       throw ex;
+                    }
                 }
             }
             document.save(file);
             document.close();
-            
-        } catch (IOException e) {
-            throw e;
-        }	
-        
+        }
     }
+    
+    
 }
